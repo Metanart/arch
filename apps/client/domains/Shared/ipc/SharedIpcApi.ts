b129e21e1ad3f6cfff84fc1fd93823f597529638
Ilya Katassonov <ilya.katassonov@gmail.com>
2026-03-14 14:19:07 +0000

    refactor: Generalize type parameter in callSharedIpcMethod for improved type safety

--- apps/client/domains/Shared/ipc/SharedIpcApi.ts
+++ apps/client/domains/Shared/ipc/SharedIpcApi.ts
@@ -1,7 +1,7 @@
 import { SHARED_IPC_CHANNELS } from '@arch/enums'
 import { IpcChannel } from '@arch/types'
 
-async function callSharedIpcMethod<Data>(channel: IpcChannel): Promise<Data> {
+async function callSharedIpcMethod<GData>(channel: IpcChannel): Promise<GData> {
   const ipcMethod = window.ipc.Shared[channel]
 
   if (!ipcMethod || typeof ipcMethod !== 'function') {
